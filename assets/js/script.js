dec88f862f0d51915c302311cf4120653b2370dc
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-07-29 14:36:15 -0500

    added vars and an event listener to my java, added cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
-const submitBtn = document.getElementById("#searchBtn")
+const submitBtn = $("#searchBtn");
+const searchCities = "";
 
-submitBtn.addEventListener("click", function() {
+var apiKey = "d899707429dae12637678613a5874634";
+var cityName = "";
+var forecastDiv = [$("#day1-div"), $("#day2-div"), $("#day3-div"), $("#day4-div"), $("#day5-div")];
 
-})
+
+submitBtn.click(function() {
+    console.log("");
+    searchCities = $("cityLookup").val().trim();
+    var previousCities = JSON.parse(localStorage.getItem("savedCities")) || []
+    previousCities.push(searchCities)
+    localStorage.setItem("savedCities", JSON.stringify(previousCities))
+    getLocation(searchCities)
+});
